bfea8c8595b6323fcd213c44c5a45e169dd11663
bluedemondd <65928463+bluedemondd@users.noreply.github.com>
2021-03-08 15:35:41 -0600

    prueba.html
    esta es una prueba

---
+++ ca_eng/enviar_email.php
@@ -0,0 +1,180 @@
+<?php
+if(isset($_POST['email'])) {
+ 
+    // Edita las líneas siguientes con tu dirección de correo y asunto
+ 
+    $email_to = "
+    contacto@cienciaplicada.org";
+ 
+    $email_subject = "Notificación sitio web";   
+ 
+    function died($error) {
+ 
+        // si hay algún error, el formulario puede desplegar su mensaje de aviso
+ 
+        echo "Lo sentimos, hay un error en sus datos y el formulario no puede ser enviado. ";
+ 
+        echo "Detalle de los errores.<br /><br />";
+        
+        echo $error."<br /><br />";
+ 
+        echo "Porfavor corrije los errores e inténtelo de nuevo.<br /><br />";
+        die();
+    }
+ 
+    // Se valida que los campos del formulairo estén llenos
+ 
+    if(!isset($_POST['first_name']) ||
+ 
+        !isset($_POST['last_name']) ||
+ 
+        !isset($_POST['email']) ||
+ 
+        !isset($_POST['telephone']) ||
+ 
+        !isset($_POST['message'])) {
+ 
+        died('Lo sentimos pero parece haber un problema con los datos enviados.');       
+ 
+    }
+ //Valor "name" nos sirve para crear las variables que recolectaran la información de cada campo
+    
+    $first_name = $_POST['first_name']; // requerido
+ 
+    $last_name = $_POST['last_name']; // requerido
+ 
+    $email_from = $_POST['email']; // requerido
+ 
+    $telephone = $_POST['telephone']; // no requerido 
+
+    $message = $_POST['message']; // requerido
+ 
+    $error_message = "Error";
+
+//Verificar que la dirección de correo sea válida 
+    
+   $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
+ 
+  if(!preg_match($email_exp,$email_from)) {
+ 
+    $error_message .= 'La dirección de correo proporcionada no es válida.<br />';
+ 
+  }
+
+//Validadacion de cadenas de texto
+
+    $string_exp = "/^[A-Za-z .'-]+$/";
+
+  if(!preg_match($string_exp,$first_name)) {
+ 
+    $error_message .= 'El formato del nombre no es válido<br />';
+ 
+  }
+ 
+  if(!preg_match($string_exp,$last_name)) {
+ 
+    $error_message .= 'el formato del apellido no es válido.<br />';
+ 
+  }
+ 
+  if(strlen($message) < 2) {
+ 
+    $error_message .= 'El formato del texto no es válido.<br />';
+ 
+  }
+ 
+  if(strlen($error_message) < 0) {
+ 
+    died($error_message);
+ 
+  }
+ 
+//Plantilla de mensaje
+
+    $email_message = "Contenido del Mensaje.\n\n";
+ 
+     
+ 
+    function clean_string($string) {
+ 
+      $bad = array("content-type","bcc:","to:","cc:","href");
+ 
+      return str_replace($bad,"",$string);
+ 
+    }
+ 
+     
+ 
+    $email_message .= "Nombre: ".clean_string($first_name)."\n";
+ 
+    $email_message .= "Apellido: ".clean_string($last_name)."\n";
+ 
+    $email_message .= "Email: ".clean_string($email_from)."\n";
+ 
+    $email_message .= "Teléfono: ".clean_string($telephone)."\n";
+ 
+    $email_message .= "Mensaje: ".clean_string($message)."\n";
+  
+ 
+//Encabezados
+ 
+$headers = 'From: '.$email_from."\r\n".
+ 
+'Reply-To: '.$email_from."\r\n" .
+ 
+'X-Mailer: PHP/' . phpversion();
+ 
+@mail($email_to, $email_subject, $email_message, $headers);  
+ 
+?>
+ <!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/common.css">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;900&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/91038794b5.js" crossorigin="anonymous"></script>
+    <link rel="icon" href="../img/favicon.ico" type="image/x-icon">
+
+    <title>Contacto</title>
+  </head>
+  <body>
+<!-- Mensaje de Éxito-->
+<style>
+  body{
+    background-color: #000;
+    color: fff;
+    font-family: sans-serif;
+    text-align: center;
+  }
+</style>
+<div class="d-flex justify-content-center align-items-center my-5">
+  <img src="../img/logo_ciencia_aplicada_verde.svg" width="200px" alt="logo_ciencia_aplicada">
+</div>
+<div class="d-flex justify-content-center align-items-center h-100">
+  <div class="card" style="width: 500px;">
+    <div class="card-body">
+      <h2 class="card-title font-weight-bold color_header">Thank you so much</h2>
+      <p class="card-text">Soon we will be in contact</p>
+      <a href="index.html" class="btn btn-dark btn-lg">Close</a>
+    </div>
+  </div>
+</div>
+
+
+<!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="../js/bootstrap.min.js"></script>
+  </body>
+  </html>
+ 
+<?php 
+}
+?>
